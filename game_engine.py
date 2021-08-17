6969f7f3faecae7247ab4c2952ef08a259fea05f
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-08-17 15:47:26 -0400

    core_engine finished

--- game_engine.py
+++ game_engine.py
@@ -44,13 +44,26 @@ class Game_engine:
             return True
         return False
 
-
-        
-        
+    def getBoard(self):
+        gameState = "-------"
+        for i in range(3):
+            curLine = "|"
+            for j in range(3):
+                if (self.board[i][j] == 0):
+                    curLine += ' '
+                if (self.board[i][j] == 1):
+                    curLine += 'X'
+                if (self.board[i][j] == 2):
+                    curLine += 'O'
+                curLine += '|'
+            gameState += '\n'
+            gameState += curLine
+        gameState += '\n-------'
+        return gameState
         
         
         
 
 
 game = Game_engine()
-print(game.checkWin(0,1,1))
+print(game.getBoard())
